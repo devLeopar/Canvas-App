59385360f668f4d052c15905d2c704b43612793b
Oktar-Mac <oktarkara@gmail.com>
2023-10-11 00:52:47 +0300

    feat: ensuring the data available before even rendering react

--- src/index.tsx
+++ src/index.tsx
@@ -3,29 +3,23 @@ import * as ReactDOM from "react-dom";
 import { App } from "./components/app";
 import { EntityStore } from "./stores/entitystore";
 
-const DEMO_DATA = [
-  {
-    id: 1,
-    name: "Order",
-    x: 100,
-    y: 100,
-  },
-  {
-    id: 2,
-    name: "OrderLine",
-    x: 200,
-    y: 200,
-  },
-];
-
 const entityStore = new EntityStore();
-entityStore.loadJson(DEMO_DATA);
-
 export const EntitiesContext = createContext<EntityStore>(entityStore);
 
-ReactDOM.render(
-  <EntitiesContext.Provider value={entityStore}>
-    <App />
-  </EntitiesContext.Provider>,
-  document.getElementById("root")
-);
+(async () => {
+  try {
+    await entityStore.fetchData();
+    ReactDOM.render(
+      <EntitiesContext.Provider value={entityStore}>
+        <App />
+      </EntitiesContext.Provider>,
+      document.getElementById("root")
+    );
+  } catch (error) {
+    console.error(`App encountered an error: ${error}`);
+    ReactDOM.render(
+      <div>Error loading application. Please try again later.</div>,
+      document.getElementById("root")
+    );
+  }
+})();
