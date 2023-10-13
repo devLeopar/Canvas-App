e48266b95ade966e6382a413a4fb7ab29e642922
Oktar-Mac <oktarkara@gmail.com>
2023-10-13 14:55:26 +0300

    feat: upgrading jest config to cover all files and include testing matchers

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'], //match file names including (spec | test).(.js,.jsx,.ts,.tsx)
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'], // to auto-import jest-dom matchers
 };
