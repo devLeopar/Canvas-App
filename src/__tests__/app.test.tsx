93d1a440365d63a28a7c4008639c5102df612167
Oktar-Mac <oktarkara@gmail.com>
2023-10-13 22:09:57 +0300

    feat: testing onAddEntity on app

--- src/__tests__/app.test.tsx
+++ src/__tests__/app.test.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import App from "../components/app";
 import { EntitiesContext } from "../stores/EntitiesContext";
 import { EntityStore } from "../stores/entitystore";
 import { setupFetchMock } from "./__mocks__/fetchMocks";
+import { fireEvent } from "@testing-library/react";
 
 beforeEach(() => {
   setupFetchMock();
@@ -20,4 +21,24 @@ test("renders App component without crashing", () => {
       <App />
     </EntitiesContext.Provider>
   );
+});
+
+test("it adds a new entity when onAddEntity is triggered", async () => {
+  // Mock the prompt and alert
+  global.prompt = jest.fn(() => "test3");
+  global.alert = jest.fn(() => {});
+
+  const { getByText, debug } = render(
+    <EntitiesContext.Provider value={new EntityStore()}>
+      <App />
+    </EntitiesContext.Provider>
+  );
+
+  // The button with text "Add Entity" that calls onAddEntity when clicked.
+  const addButton = getByText("Add Entity");
+  fireEvent.click(addButton);
+
+  // Check for the mocked entity name in the rendered output
+  const newEntity = getByText("test3");
+  expect(newEntity).toBeInTheDocument();
 });
